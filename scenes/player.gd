37c904fa12f286c4aed3fc4d12d08978eccdfd33
Eyosias <75771087+eyos-ias@users.noreply.github.com>
2025-04-02 12:59:55 +0300

    bullets, aiming and firing

---
+++ scenes/player.gd
@@ -0,0 +1,88 @@
+extends CharacterBody3D
+
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+
+@export var mouse_sensitivity := 0.005
+@onready var camera_holder = $CameraHolder
+@export var world: Node3D
+
+@onready var raycast: RayCast3D = $RayCast3D
+
+@onready var crosshair: Sprite3D = $RayCast3D/crosshair
+@onready var aim_sphere: MeshInstance3D = $RayCast3D/aim_sphere
+
+@export var bullet_scene: PackedScene
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			print("toggle visibility")
+			aim_sphere.visible = !aim_sphere.visible
+			crosshair.visible = !crosshair.visible
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			print("Mouse wheel up")
+			aim_sphere.position.x += 0.05
+			crosshair.position.x += 0.05
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			print("Mouse wheel down")
+			aim_sphere.position.x -= 0.05
+			crosshair.position.x +=0.05
+
+	if event is InputEventMouseMotion:
+		rotate_y(event.relative.x * mouse_sensitivity)
+		camera_holder.rotate_y(-event.relative.x * mouse_sensitivity)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, -10, 20 * delta)
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_released("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.is_action_just_released("shoot"):
+		shoot_bullet()
+	
+	var input_dir := Input.get_vector("up", "down", "right", "left")
+	if input_dir != Vector2.ZERO:
+		var isometric_dir := Vector3(
+			input_dir.x - input_dir.y,
+			0,
+			input_dir.x + input_dir.y
+		).normalized()
+		velocity.x = isometric_dir.x * SPEED
+		velocity.z = isometric_dir.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func shoot_bullet():
+	if world:
+		print("can shoot")
+		var bullet_instance = bullet_scene.instantiate()
+		bullet_instance.global_transform = raycast.global_transform
+		
+		world.add_child(bullet_instance)
+	else:
+		print("can't shoot")
+
+# func shoot_bullet():
+# 	if bullet_scene and canShoot:
+# 		# print("this is shooting")
+# 		shooting_sfx.play()
+# 		var bullet_instance = bullet_scene.instantiate()
+# 		bullet_instance.global_transform = bulletSpawner.global_transform
+# 		get_parent().add_child(bullet_instance)
+# 		# add_child(bullet_instance)
+# 		canShoot = false
+		
+# 		gun.get_node("AnimationPlayer").play("shoot")
+		
+# 		shootTimer.start()
---
+++ scripts/bullet.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+@export var SPEED = 40.0
+
+@onready var mesh = $MeshInstance3D
+@onready var ray = $RayCast3D
+
+#signal give_stat(stat_type: int)
+#signal enemy_hit(enemy)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	rotate_y(deg_to_rad(90))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.basis * Vector3(0, 0, SPEED) * delta
+
+
+func _on_timer_timeout() -> void:
+	# print("queue freed")
+	queue_free()
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	print("i hit an area boss")
+	
+	queue_free()
+	
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("i hit something boss")
+	if (body.is_in_group("player")):
+		print("print player hit")
+	#queue_free()
